40d80f5af217bb032cae269df26089726ed151df
SergiNaranjo <sergi.naranjo@enti.cat>
2024-11-30 11:18:00 +0100

    Changes in combat

--- RPGRPGRPG/RPGRPGRPG/main.cpp
+++ RPGRPGRPG/RPGRPGRPG/main.cpp
@@ -5,6 +5,7 @@
 #include "Consts.h"
 #include "Character.h"
 #include "Enemy.h"
+#include "Dungeon.cpp"
 
 void main()
 {
@@ -15,6 +16,8 @@ void main()
 	stats.health = rand() % (MAX_HP_ENEMY - 1 - MIN_HP_ENEMY) + MIN_HP_ENEMY;
 	int actualEnemyHealth = stats.health;
 	stats.stamina = rand() % (MAX_STAMINA_ENEMY - 1 - MIN_STAMINA_ENEMY) + MIN_STAMINA_ENEMY;
+	int actualEnemyStamina = stats.stamina;
+
 	printf("------COMBAT------\n");
 	printf("\n");
 
@@ -29,5 +32,18 @@ void main()
 		printf("[ = = = ] %d HP\n", stats.health);
 	}
 	
-	printf("%d Stamina", stats.stamina);
+	if (actualEnemyStamina == stats.stamina)
+	{
+		printf("[ < < < < < < ] %d Stamina\n", stats.stamina);
+	}
+	else if (actualEnemyStamina == stats.stamina / 2)
+	{
+		printf("[ < < < ] %d Stamina\n", stats.stamina);
+	}
+	
+	printf("\n");
+	printf("----------------------\n");
+	printf("\n");
+
+	printf("--Player--\n");
 }
